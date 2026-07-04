1a7d2e1f3b3cf74bf8b5bc6ca4fe158baeecca40
Vinura-Prabath <vinurakumarathunga8866@gmail.com>
2026-07-04 09:49:16 +0530

    Comment out unused Customer class and related code in app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // console.log("Hello, World!");
 
+// class Customer{
+//     name;
+//     age;
+//     address;
+
+//     constructor(name, age, address){
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+// }
+
+// let customer1 = new Customer("Sharadha", 20, "Bangalore");
+// console.log(customer1);
+
+// ---------------------------------------------
+
 let customer2={
     name: "John", 
     age: 30,
