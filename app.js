e44188e60ba26dc549dd2202ccfc6a96ed97e7b2
Vinura-Prabath <vinurakumarathunga8866@gmail.com>
2026-07-04 11:20:19 +0530

    Refactor customer2 object and log details; update HTML structure for heading

--- app.js
+++ app.js
@@ -17,49 +17,58 @@
 
 // ---------------------------------------------
 
-let customer2={
-    name: "John", 
-    age: 30,
-    address: "Kandy",
-    salary: 50000,
-    items:["item1", "item2", "item3"],
+// let customer2={
+//     name: "John", 
+//     age: 30,
+//     address: "Kandy",
+//     salary: 50000,
+//     items:["item1", "item2", "item3"],
 
-    items:[
-        {   
-            id: 1,
-            name: "item1",
-            price: 1000,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 100
-                }
-            ]
-        },{
-            id: 2,
-            name: "item2",
-            price: 2000
-        },{
-            id: 3,
-            name: "item3",
-            price: 3000
-        }
-        ],
+//     items:[
+//         {   
+//             id: 1,
+//             name: "item1",
+//             price: 1000,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 100
+//                 }
+//             ]
+//         },{
+//             id: 2,
+//             name: "item2",
+//             price: 2000
+//         },{
+//             id: 3,
+//             name: "item3",
+//             price: 3000
+//         }
+//         ],
 
-    father:{
-        name: "Peter",
-        age: 60,
-        address: "Colombo"
-    }
-};
+//     father:{
+//         name: "Peter",
+//         age: 60,
+//         address: "Colombo"
+//     }
+// };
 
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items);
-console.log(customer2.father);
-console.log(customer2.father.name);
-console.log(customer2.items[2]);
-console.log(customer2.items[0].variants[0].price);
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items);
+// console.log(customer2.father);
+// console.log(customer2.father.name);
+// console.log(customer2.items[2]);
+// console.log(customer2.items[0].variants[0].price);
+
+
+// document.write("<h1>Hello</h1>");
+
+console.log(document.title);
+
+let heading = document.getElementById("Heading");
+
+console.log(heading);
